09b4f3cab8c462a3281ea6c477301c758428fca0
samira6674 <62030598+samira6674@users.noreply.github.com>
2021-09-16 22:59:22 -0700

    Update pascals-triangle.js

--- pascals-triangle.js
+++ pascals-triangle.js
@@ -1,1 +1,19 @@
-const addRow = () => {};
+const addRow = (row) => {
+    if(row === 0)
+    return[];
+    
+    const triangle = [];
+    for(let i=0; i<row; i++){
+        triangle[i] = [];
+        for(let j=0; j<=i ;j++){
+            if (j === 0) {
+                result[j][i] = 1
+            } else if (i === 0 || i === j) {
+                result[j][i] = 1
+            } else {
+                result[j][i] = result[j - 1][i - 1] + result[j - 1][i]
+          }
+        }
+      }
+      return result;
+};
